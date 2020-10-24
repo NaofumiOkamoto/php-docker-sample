2be00b902a813c667fefba984eb8a28e17ad633b
岡本尚文 <okamotonaofumi@okamo.local>
2020-10-24 10:26:08 +0900

    first commit

---
+++ src/Controller/TestsController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Sample Controller
+ */
+class TestsController extends AppController
+{
+    public function index()
+    {
+    }
+
+}
